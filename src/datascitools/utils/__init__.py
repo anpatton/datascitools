22eebf5491f84fe7a6fc93384aa79cec47b5f9f7
Andrew Patton <anpatt7@gmail.com>
2022-10-03 15:49:35 -0700

    add utility function to expand lists

--- src/datascitools/utils/__init__.py
+++ src/datascitools/utils/__init__.py
@@ -5,17 +5,15 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 from jenkspy import jenks_breaks
 from numpy.typing import ArrayLike
-
+from itertools import chain
 
 def cbind(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     """R clone for column binding"""
     return pd.concat([df1.reset_index(drop=True), df2], axis=1)
 
-
 def read_pickle(file: str):
     """Read pickle object from file."""
     return pickle.load(open(file, "rb"))
-
 
 def write_pickle(object, file: str) -> bool:
     """Write pickle object to file."""
@@ -24,13 +22,16 @@ def write_pickle(object, file: str) -> bool:
         pickle.dump(object, f)
     return True
 
-
 def rmse(pred: ArrayLike, actual: ArrayLike, digits: int = 3) -> float:
     """Rounded RMSE"""
     return np.round(mean_squared_error(pred, actual, squared=False), digits)
 
-
 def jenks_discretize(data, n_classes: int) -> np.array:
+    """Jenks breaks of vector"""
     breaks = jenks_breaks(data, n_classes=n_classes)
     breaks[-1] = np.inf
     return np.digitize(data, breaks)
+
+def expand_lists(item_repeats, *lists_of_items):
+    """Provide # of repetitions for each item in list(s)"""
+    return [list(chain([[lists_of_items]*item_repeats for lists_of_items, item_repeats in zip(lists_of_items, item_repeats)])) for item_repeats in lists_of_items]
